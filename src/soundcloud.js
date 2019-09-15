88dbf59d927fa99e5564ed03b92e2a3b39499466
nicknish <nickjnish@gmail.com>
2019-09-15 13:13:56 -0700

    PR Suggestions: Update code style of Soundcloud html and url generation to 1-line template literal

--- src/soundcloud.js
+++ src/soundcloud.js
@@ -4,28 +4,10 @@ export const shouldTransform = string =>
   new URL(string).host === 'soundcloud.com';
 
 const getSoundcloudIFrameSrc = string =>
-  `https://w.soundcloud.com/player` +
-  `?url=${string}` +
-  `&color=ff5500` +
-  `&auto_play=false` +
-  `&hide_related=true` +
-  `&show_comments=true` +
-  `&show_user=true` +
-  `&show_reposts=false` +
-  `&show_teaser=false` +
-  `&visual=true`;
+  `https://w.soundcloud.com/player?url=${string}&color=ff5500&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false&visual=true`;
 
 export const getHTML = string => {
   const iframeUrl = getSoundcloudIFrameSrc(string);
 
-  return (
-    `<iframe` +
-    ` width="100%"` +
-    ` height="300"` +
-    ` scrolling="no"` +
-    ` frameborder="no"` +
-    ` allow="autoplay"` +
-    ` src=${iframeUrl}` +
-    `></iframe>`
-  );
+  return `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src=${iframeUrl}></iframe>`;
 };
