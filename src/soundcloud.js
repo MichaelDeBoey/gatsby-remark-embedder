0a2dff564648234cd5e3ec5605a37ac4061b3bcb
nicknish <nickjnish@gmail.com>
2019-09-15 12:15:05 -0700

    PR suggestion: Use implicit function returns

--- src/soundcloud.js
+++ src/soundcloud.js
@@ -1,23 +1,19 @@
 import { URL } from 'url';
 
-export const shouldTransform = string => {
-  return new URL(string).host === 'soundcloud.com';
-};
+export const shouldTransform = string =>
+  new URL(string).host === 'soundcloud.com';
 
-const getSoundcloudIFrameSrc = string => {
-  return (
-    `https://w.soundcloud.com/player/` +
-    `?url=${encodeURIComponent(string)}` +
-    `&color=%23ff5500` +
-    `&auto_play=false` +
-    `&hide_related=false` +
-    `&show_comments=true` +
-    `&show_user=true` +
-    `&show_reposts=false` +
-    `&show_teaser=true` +
-    `&visual=true`
-  );
-};
+const getSoundcloudIFrameSrc = string =>
+  `https://w.soundcloud.com/player/` +
+  `?url=${encodeURIComponent(string)}` +
+  `&color=%23ff5500` +
+  `&auto_play=false` +
+  `&hide_related=false` +
+  `&show_comments=true` +
+  `&show_user=true` +
+  `&show_reposts=false` +
+  `&show_teaser=true` +
+  `&visual=true`;
 
 export const getHTML = string => {
   const iframeUrl = getSoundcloudIFrameSrc(string);
