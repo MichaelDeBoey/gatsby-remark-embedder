2296d4cbf19cd1f615dfbc9308131aacb1abb5b8
anuraghazra <hazru.anurag@gmail.com>
2019-10-10 12:29:23 +0530

    updated CodePen.js shouldTransform to use regex for matching urls

--- src/CodePen.js
+++ src/CodePen.js
@@ -1,8 +1,10 @@
 import { URL } from 'url';
 
-export const shouldTransform = string => new URL(string).host == 'codepen.io';
+export const shouldTransform = string =>
+  !!new URL(string).pathname.match(/(\/embed|\/pen)/);
 
 export const getHTML = string => {
   const iframeUrl = string.replace('/pen/', '/embed/');
+
   return `<iframe src="${iframeUrl}" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"></iframe>`;
 };
