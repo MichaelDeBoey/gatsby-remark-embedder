927076add2c0cbb095c43fec5945977b4b6bca69
anuraghazra <hazru.anurag@gmail.com>
2019-10-10 18:48:54 +0530

    fixed logic for shouldTransform and getHTML replaced with /embed/preview/

--- src/CodePen.js
+++ src/CodePen.js
@@ -1,10 +1,15 @@
 import { URL } from 'url';
 
-export const shouldTransform = string =>
-  !!new URL(string).pathname.match(/\/pen/);
+export const shouldTransform = url => {
+  const urlObj = new URL(url);
+  if (urlObj.host === 'codepen.io') {
+    return !!urlObj.pathname.match(/\/pen\/.*\w/);
+  }
+  return false;
+};
 
 export const getHTML = string => {
-  const iframeUrl = string.replace('/pen/', '/embed/');
+  const iframeUrl = string.replace('/pen/', '/embed/preview/');
 
   return `<iframe src="${iframeUrl}" style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"></iframe>`;
 };
