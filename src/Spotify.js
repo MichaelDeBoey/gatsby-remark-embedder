30cdae0f060808d51fac0926a1fd7755dd90ad6d
jyash97 <jyash97@gmail.com>
2019-11-02 12:31:54 +0530

    fix: tests and refactor code

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,38 +1,34 @@
 import { URL } from 'url';
 
+const podcastTypes = [
+  'album',
+  'artist',
+  'episode',
+  'show',
+  'track',
+  'playlist',
+];
+
 export const shouldTransform = url => {
   const { host, pathname } = new URL(url);
-
-  return (
-    host === 'open.spotify.com' &&
-    (pathname.includes('/album/') ||
-      pathname.includes('/artist/') ||
-      pathname.includes('/episode/') ||
-      pathname.includes('/show/') ||
-      pathname.includes('/playlist/') ||
-      pathname.includes('/track/'))
-  );
+  const pathnameFields = pathname.split('/');
+  const type =
+      (pathnameFields && pathnameFields[1] && pathnameFields[1].toLowerCase()) ||
+      null;
+  return host === 'open.spotify.com' && podcastTypes.includes(type);
 };
 
 export const getSpotifyIFrameSrc = urlString => {
   const { pathname } = new URL(urlString);
   const type = pathname.split('/')[1].toLowerCase();
 
-  switch (type) {
-    case 'album':
-    case 'artist':
-    case 'episode':
-    case 'show':
-    case 'track':
-    case 'playlist':
-      return urlString.replace(type, `embed/${type}`);
-    default:
-      return null;
-  }
+  return (
+      podcastTypes.includes(type) && urlString.replace(type, `embed/${type}`)
+  );
 };
 
 export const getHTML = url => {
   const iframeSrc = getSpotifyIFrameSrc(url);
 
-  return `<iframe src="${iframeSrc}" style="width:100%; height:240px; border:0;" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
+  return `<iframe src="${iframeSrc}" width="100%" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
 };
