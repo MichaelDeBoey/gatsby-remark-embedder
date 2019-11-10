0f7064a9b3967c97be9579a18a19411f4749e4f3
jyash97 <jyash97@gmail.com>
2019-11-10 19:10:11 +0530

    fix: requested change

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,30 +1,33 @@
 import { URL } from 'url';
 
-const podcastTypes = [
-  'album',
-  'artist',
-  'episode',
-  'show',
-  'track',
-  'playlist',
-];
+const includesSomeOfArray = (string, array) =>
+  array.some(item => string.startsWith(item));
 
 export const shouldTransform = url => {
   const { host, pathname } = new URL(url);
-  const pathnameFields = pathname.split('/');
-  const type =
-      (pathnameFields && pathnameFields[1] && pathnameFields[1].toLowerCase()) ||
-      null;
-  return host === 'open.spotify.com' && podcastTypes.includes(type);
+
+  return (
+    host === 'open.spotify.com' &&
+    includesSomeOfArray(pathname, [
+      '/album/',
+      '/artist/',
+      '/episode/',
+      '/show/',
+      '/track/',
+      '/playlist/',
+    ])
+  );
 };
 
 export const getSpotifyIFrameSrc = urlString => {
   const { pathname } = new URL(urlString);
   const type = pathname.split('/')[1].toLowerCase();
 
-  return (
-      podcastTypes.includes(type) && urlString.replace(type, `embed/${type}`)
-  );
+  if (['episode', 'show'].includes(type)) {
+    return urlString.replace(type, `embed-podcast/${type}`);
+  }
+
+  return urlString.replace(type, `embed/${type}`);
 };
 
 export const getHTML = url => {
