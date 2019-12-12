a78213a00777510176e7217d4c94dc3957cfd610
james simone <jamirus99@gmail.com>
2019-12-12 15:28:12 -0500

    fixing linting

--- src/Streamable.js
+++ src/Streamable.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 export const shouldTransform = url => /^https?:\/\/streamable\.com\//.test(url);
 
-export const getHTML = async url =>
+export const getHTML = url =>
   fetch(`https://api.streamable.com/oembed.json?url=${url}`)
     .then(res => res.json().then(streamable => streamable.html))
     .catch(err => err);
