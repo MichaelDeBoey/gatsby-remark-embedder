bdc0abd6b0b3adfa6a3c31849d28662a1cd60d79
Michaël De Boey <info@michaeldeboey.be>
2019-12-20 11:05:35 +0100

    Move CodeSandbox plugin test to specific test file

--- src/__tests__/plugin.js
+++ src/__tests__/plugin.js
@@ -78,17 +78,6 @@ describe('gatsby-remark-embedder', () => {
     `);
   });
 
-  it('can transform CodeSandbox links', async () => {
-    const markdownAST = getMarkdownASTForFile('CodeSandbox');
-
-    const processedAST = await plugin({ cache, markdownAST });
-
-    expect(remark.stringify(processedAST)).toMatchInlineSnapshot(`
-      "<iframe src=\\"https://codesandbox.io/embed/ynn88nx9x?view=split\\" style=\\"width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;\\" allow=\\"geolocation; microphone; camera; midi; vr; accelerometer; gyroscope; payment; ambient-light-sensor; encrypted-media; usb\\" sandbox=\\"allow-modals allow-forms allow-popups allow-scripts allow-same-origin\\"></iframe>
-      "
-    `);
-  });
-
   it('can transform Slides links', async () => {
     const markdownAST = getMarkdownASTForFile('Slides');
 
