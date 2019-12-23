a231ac61f9e729423b2b5df28d938a7317c2a8bd
james simone <jamirus99@gmail.com>
2019-12-23 14:39:52 -0500

    Updating to simply use array.includes

--- src/transformers/Streamable.js
+++ src/transformers/Streamable.js
@@ -3,9 +3,6 @@ import fetch from 'node-fetch';
 
 const getTrimmedPathName = pathname => pathname.replace(/^\/|\/+$/g, '');
 
-const includesSomeOfArray = (string, array) =>
-  array.some(item => string.includes(item));
-
 export const shouldTransform = url => {
   const { host, pathname } = new URL(url);
   const trimmedPathName = getTrimmedPathName(pathname).split('/');
@@ -13,12 +10,12 @@ export const shouldTransform = url => {
   return (
     (['streamable.com', 'www.streamable.com'].includes(host) &&
       trimmedPathName.length === 1 &&
-        !['documentation', 'login', 'recover', 'settings', 'signup'].includes(
-          trimmedPathName[0]
-        )) ||
+      !['documentation', 'login', 'recover', 'settings', 'signup'].includes(
+        trimmedPathName[0]
+      )) ||
     (trimmedPathName.length > 1 &&
       trimmedPathName.length <= 3 &&
-      includesSomeOfArray(trimmedPathName[0], ['e', 'g', 'o', 's', 't']))
+      ['e', 'g', 'o', 's', 't'].includes(trimmedPathName[0]))
   );
 };
 
