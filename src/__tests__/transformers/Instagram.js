c228f783aca6b87d187088a3e32675a7d0b2d6a1
FabioRosado <fabioglrosado@gmail.com>
2019-12-28 19:18:26 +0000

    Add instagram transformer

---
+++ src/__tests__/transformers/Instagram.js
@@ -0,0 +1,97 @@
+import cases from 'jest-in-case';
+import fetchMock from 'node-fetch';
+
+import plugin from '../..';
+import { getHTML, shouldTransform } from '../../transformers/Instagram';
+
+import { cache, getMarkdownASTForFile, parseASTToMarkdown } from '../helpers';
+
+jest.mock('node-fetch', () => jest.fn());
+
+const mockFetch = html =>
+  fetchMock.mockResolvedValue({ json: () => Promise.resolve({ html }) });
+
+beforeEach(() => {
+  fetchMock.mockClear();
+});
+
+cases(
+  'url validation',
+  ({ url, valid }) => {
+    expect(shouldTransform(url)).toBe(valid);
+  },
+  {
+    'non-Instagram url': {
+      url: 'https://not-an-instagram-url.com',
+      valid: false,
+    },
+    "non-Instagram url ending with 'instagram.com'": {
+      url: 'https://this-is-not-instagram.com',
+      valid: false,
+    },
+    "non-Instagram url ending with 'instagram.com' and having '/p/'": {
+      url: 'https://this-is-not-instagram.com/p/123',
+      valid: false,
+    },
+    "non-Instagram url ending with 'instagr.am/'": {
+      url: 'https://this-is-not-instagr.am/',
+      valid: false,
+    },
+    'profile url': {
+      url: 'https://instagram.com/FabioRosado',
+      valid: false,
+    },
+    'image url': {
+      url: 'https://instagram.com/p/B39qQ_GJ_kE/',
+      valid: true,
+    },
+    "status url having 'www' subdomain": {
+      url: 'https://www.instagram.com/p/B39qQ_GJ_kE/',
+      valid: true,
+    },
+    "image url with 'instagr.am'": {
+      url: 'https://instagram.com/p/B39qQ_GJ_kE/',
+      valid: true,
+    },
+    "status url having 'www' subdomain with 'instagr.am": {
+      url: 'https://www.instagr.am/p/B39qQ_GJ_kE/',
+      valid: true,
+    },
+  }
+);
+
+test('Gets the correct Instagram iframe', async () => {
+  mockFetch(
+    `<blockquote class=\\"instagram-media\\" data-instgrm-captioned data-instgrm-permalink data-instgrm-version=\\"12\\"><div> <a href> <div><svg></svg></div><div> View this post on Instagram</div><div style=\\"mocked-styles\\"></div> </div></div></a> <p> <a href>description</a></p> <p>A post shared by <a href> Fábio  Rosado</a> (@fabiorosado) on <time datetime>date</time></p></div></blockquote>`
+  );
+
+  const html = await getHTML('https://www.instagram.com/p/B39qQ_GJ_kE/');
+
+  expect(html).toMatchInlineSnapshot(
+    `"<blockquote class=\\"instagram-media\\" data-instgrm-captioned data-instgrm-permalink data-instgrm-version=\\"12\\"><div> <a href> <div><svg></svg></div><div> View this post on Instagram</div><div style=\\"mocked-styles\\"></div> </div></div></a> <p> <a href>description</a></p> <p>A post shared by <a href> Fábio  Rosado</a> (@fabiorosado) on <time datetime>date</time></p></div></blockquote>"`
+  );
+});
+
+test('Plugin can transform Instagram links', async () => {
+  mockFetch(
+    `<blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink data-instgrm-version="12"><div> <a href> <div><svg></svg></div><div> View this post on Instagram</div><div style="mocked-styles"></div> </div></div></a> <p> <a href>description</a></p> <p>A post shared by <a href> Fábio  Rosado</a> (@fabiorosado) on <time datetime>date</time></p></div></blockquote>`
+  );
+  const markdownAST = getMarkdownASTForFile('Instagram');
+
+  const processedAST = await plugin({ cache, markdownAST });
+
+  expect(parseASTToMarkdown(processedAST)).toMatchInlineSnapshot(`
+    "<https://not-an-instagram-url.com>
+
+    <https://this-is-not-instagram.com>
+
+    <https://this-is-not-instagram.com/foobar/status/123>
+
+    <https://www.instagram.com/p/B39qQ_GJ_kE/>
+
+    <blockquote class=\\"instagram-media\\" data-instgrm-captioned data-instgrm-permalink data-instgrm-version=\\"12\\"><div> <a href> <div><svg></svg></div><div> View this post on Instagram</div><div style=\\"mocked-styles\\"></div> </div></div></a> <p> <a href>description</a></p> <p>A post shared by <a href> Fábio  Rosado</a> (@fabiorosado) on <time datetime>date</time></p></div></blockquote>
+
+    <blockquote class=\\"instagram-media\\" data-instgrm-captioned data-instgrm-permalink data-instgrm-version=\\"12\\"><div> <a href> <div><svg></svg></div><div> View this post on Instagram</div><div style=\\"mocked-styles\\"></div> </div></div></a> <p> <a href>description</a></p> <p>A post shared by <a href> Fábio  Rosado</a> (@fabiorosado) on <time datetime>date</time></p></div></blockquote>
+    "
+  `);
+});
---
+++ src/transformers/Instagram.js
@@ -0,0 +1,25 @@
+import { URL } from 'url';
+import fetch from 'node-fetch';
+
+export const shouldTransform = url => {
+  const { host, pathname } = new URL(url);
+
+  return (
+    [
+      'http://instagr.am/',
+      'instagr.am/',
+      'www.instagram.com',
+      'instagram.com',
+    ].includes(host) && pathname.includes('/p/')
+  );
+};
+
+export const getHTML = url =>
+  fetch(`https://api.instagram.com/oembed?url=${url}&omitscript=true`)
+    .then(r => r.json())
+    .then(r =>
+      [r.html]
+        .map(s => s.replace(/\?&amp;utm_campaign*?/g, ''))
+        .join('')
+        .trim()
+    );
