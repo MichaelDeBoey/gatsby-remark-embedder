62bafe331215b4f1dc2eade771680bbba2abf346
Nicky Meuleman <nicky.meuleman@gmail.com>
2020-01-24 14:52:09 +0100

    add instagram custom parameter support

--- src/transformers/Instagram.js
+++ src/transformers/Instagram.js
@@ -2,6 +2,8 @@ import { fetchOEmbedData } from './utils';
 
 export const name = 'instagram';
 
+export const name = 'instagram';
+
 export const shouldTransform = url => {
   const { host, pathname } = new URL(url);
   return (
@@ -14,7 +16,11 @@ export const shouldTransform = url => {
   );
 };
 
+<<<<<<< HEAD
 export const buildUrl = (url, options = {}) => {
+=======
+export const getHTML = (url, options) => {
+>>>>>>> add instagram custom parameter support
   // https://www.instagram.com/developer/embedding/#oembed
   const urlObj = new URL(`https://api.instagram.com/oembed`);
   urlObj.search = new URLSearchParams({
@@ -22,10 +28,17 @@ export const buildUrl = (url, options = {}) => {
     omitscript: true,
     ...options,
   });
+<<<<<<< HEAD
   return urlObj.toString();
 };
 
 export const getHTML = (url, options) => {
   const oEmbedUrl = buildUrl(url, options);
   return fetchOEmbedData(oEmbedUrl).then(({ html }) => html);
+=======
+
+  return fetch(urlObj.toString())
+    .then(({ json }) => json())
+    .then(({ html }) => html);
+>>>>>>> add instagram custom parameter support
 };
