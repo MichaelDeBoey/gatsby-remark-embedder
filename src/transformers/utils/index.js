9121d5bb8728553cf30800c65e0594d3982561ca
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-01-27 21:00:48 +0100

    feat: Add retry mechanism for network requests (#75)
    * feat: add retry mechanism for network requests
    
    This should make the plugin more resilient to network errors. It adds up to 3 retries (with increasing timeouts between them).
    
    * use implicit return
    
    * Cleanup imports
    
    * Update fetch-retry
    
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- src/transformers/utils/index.js
+++ src/transformers/utils/index.js
@@ -1,6 +1,12 @@
+import wrapFetch from 'fetch-retry';
 import fetch from 'node-fetch';
 
-export const fetchOEmbedData = url => fetch(url).then(data => data.json());
+const fetchWithRetries = wrapFetch(fetch);
+export const fetchOEmbedData = url =>
+  fetchWithRetries(url, {
+    retries: 3,
+    retryDelay: attempt => 2 ** attempt * 1000,
+  }).then(data => data.json());
 
 export const getTrimmedPathName = pathname =>
   // Trim leading and trailing slashes
