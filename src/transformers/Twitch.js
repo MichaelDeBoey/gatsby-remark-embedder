d3f4227bbb96f8c9a9237827431ca67112581f1d
Michaël De Boey <info@michaeldeboey.be>
2020-02-10 21:32:03 +0100

    Extract getTwitchIFrameSrc function

--- src/transformers/Twitch.js
+++ src/transformers/Twitch.js
@@ -6,10 +6,13 @@ export const shouldTransform = url => {
   return ['player.twitch.tv', 'www.twitch.tv', 'twitch.tv'].includes(host);
 };
 
-export const getHTML = url => {
-  const iframeUrl = url
+export const getTwitchIFrameSrc = urlString =>
+  urlString
     .replace(/https?:\/\/(?:www.)?twitch.tv/g, 'https://player.twitch.tv')
     .replace(/twitch.tv\/(\w+)/g, 'twitch.tv/?channel=$1');
+
+export const getHTML = url => {
+  const iframeUrl = getTwitchIFrameSrc(url);
 
   return `<iframe src=${iframeUrl} height="600" width="400" frameborder="0" scrolling="no" allowfullscreen="true"></iframe>`;
 };
