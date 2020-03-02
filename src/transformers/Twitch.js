8738cefd60bb013dbb68f24cef0a687a0ec79756
FabioRosado <fabioglrosado@gmail.com>
2020-03-02 15:32:16 +0000

    Add isValid function to check url validation rules, refactor getTwitchIFrameSrc

--- src/transformers/Twitch.js
+++ src/transformers/Twitch.js
@@ -1,10 +1,26 @@
 import { URL } from 'url';
-import { includesSomeOfArray } from './utils';
 
-const ignoredPaths = ['/followers', '/settings', '/videos'];
+export const isValid = url => {
+  const { pathname } = new URL(url);
+  const pathnameParts = pathname.split('/');
+
+  if (url.match('embed') || pathnameParts[2] === 'videos') {
+    return false;
+  }
+
+  if (url.match(/collection|video|channel|channel|clip/g)) {
+    return true;
+  }
+
+  if (pathnameParts.length == 2 && pathnameParts[1]) {
+    return true;
+  }
+
+  return false;
+};
 
 export const shouldTransform = url => {
-  const { host, pathname } = new URL(url);
+  const { host } = new URL(url);
 
   return (
     [
@@ -12,9 +28,7 @@ export const shouldTransform = url => {
       'www.twitch.tv',
       'twitch.tv',
       'clips.twitch.tv',
-    ].includes(host) &&
-    !includesSomeOfArray(pathname, ['followers', 'settings']) &&
-    !pathname.endsWith('videos')
+    ].includes(host) && isValid(url)
   );
 };
 
@@ -22,20 +36,17 @@ export const getTwitchIFrameSrc = urlString => {
   const { href } = new URL(urlString);
 
   const location = href.match(/([^/=]*)\/*$/g)[0];
+  const urlType = urlString.match(/collection|video|clip/);
 
-  if (urlString.match(/clip/g)) {
+  if (urlType === null) {
+    return `https://player.twitch.tv?channel=${location}`;
+  }
+
+  if (urlType[0] === 'clip') {
     return `https://clips.twitch.tv/embed?clip=${location}`;
   }
 
-  if (urlString.match(/collection/g)) {
-    return `https://player.twitch.tv?collection=${location}`;
-  }
-
-  if (urlString.match(/video/g)) {
-    return `https://player.twitch.tv?video=${location}`;
-  }
-
-  return `https://player.twitch.tv?channel=${location}`;
+  return `https://player.twitch.tv?${urlType[0]}=${location}`;
 };
 
 export const getHTML = url => {
