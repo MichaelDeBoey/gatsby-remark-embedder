7377197cceba6cf430457f4b8b819978947106a7
Sean McNamara <me@seanmcn.com>
2020-10-29 13:20:58 +0000

    feat(YouTube): Export Youtube transformer name so data passed in service config comes through.

--- src/transformers/YouTube.js
+++ src/transformers/YouTube.js
@@ -50,3 +50,5 @@ export const getHTML = (url, { width = '100%', height = '315' }) => {
 
   return `<iframe width="${width}" height="${height}" src="${iframeSrc}" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>`;
 };
+
+export const name = 'YouTube';
