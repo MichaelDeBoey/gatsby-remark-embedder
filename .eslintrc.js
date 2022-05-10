1f605869c9c9d5a43989dba7aa1b79d1324d7e32
Michaël De Boey <info@michaeldeboey.be>
2022-05-10 16:58:12 +0200

    fix: fix export

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,21 @@
 module.exports = {
   extends: './node_modules/kcd-scripts/eslint.js',
+  rules: {
+    'import/no-import-module-exports': [
+      'error',
+      { exceptions: ['**/src/index.js'] },
+    ],
+  },
+  overrides: [
+    {
+      files: [
+        'src/__tests__/CustomTransformer.js',
+        'src/__tests__/plugin.js',
+        'src/__tests__/transformers/*.js',
+      ],
+      rules: {
+        'import/default': 'off',
+      },
+    },
+  ],
 };
